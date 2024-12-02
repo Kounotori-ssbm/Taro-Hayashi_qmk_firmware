8bffc899cef2f58359abdc9c9084267e98e07e4e
Kounotori <19996147+Kounotori-ssbm@users.noreply.github.com>
2024-12-02 15:23:47 +0900

    Mac 用のキーマップをそのまま日本語配列設定の Windows でも使用できるよう補正する処理を追加

--- keyboards/tarohayashi/killerwhale/duo/keymaps/default/keymap.c
+++ keyboards/tarohayashi/killerwhale/duo/keymaps/default/keymap.c
@@ -14,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_T, KC_U, KC_V, KC_W, KC_X,
                     KC_Y,
         // 側面スイッチ
-        KC_1, KC_2,                
+        KC_1, KC_2,
         // 十字キーorジョイスティック     // ジョイスティックスイッチ
-        KC_3, KC_4, KC_5, KC_6,       KC_7,      
+        KC_3, KC_4, KC_5, KC_6,       KC_7,
         // 追加スイッチ                 // トグルスイッチ
         KC_8, KC_9,                   XXXXXXX,
         // 右手
@@ -26,16 +26,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_T, KC_U, KC_V, KC_W, KC_X,
                     KC_Y,
         // 側面スイッチ
-        KC_1, KC_2,                
+        KC_1, KC_2,
         // 十字キーorジョイスティック     // ジョイスティックスイッチ
-        KC_3, KC_4, KC_5, KC_6,       KC_7,      
+        KC_3, KC_4, KC_5, KC_6,       KC_7,
         // 追加スイッチ                 // トグルスイッチ
         KC_8, KC_9,                   XXXXXXX
     )
 };
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   { 
+    [0] =   {
         ENCODER_CCW_CW(KC_1, KC_2),
         ENCODER_CCW_CW(KC_3, KC_4),
         ENCODER_CCW_CW(KC_5, KC_6),
@@ -49,16 +49,74 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     }
 };
 
+
+// [Kounotori カスタマイズ]
+// https://github.com/qmk/qmk_firmware/blob/master/docs/custom_quantum_functions.md#programming-the-behavior-of-any-keycode-idprogramming-the-behavior-of-any-keycode
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (detected_host_os() == OS_MACOS || detected_host_os() == OS_IOS) {
+        return true; // Mac では今のところ手を加えるものはない
+    }
+
+    switch (keycode) {
+        case LT(LOWER, KC_GRAVE):
+            if (!record->tap.count) {
+                return true; // Hold の場合はそのまま処理
+            }
+
+            // Tap の場合は日本語配列設定の Windows でキーコードと入力されるキーを一致させる
+            if (record->event.pressed) {
+                register_code(KC_BACKSLASH);
+            } else {
+                unregister_code(KC_BACKSLASH);
+            }
+            return false; // Skip all further processing of this key
+        case LCTL_T(KC_SPACE):
+            if (record->tap.count) {
+                return true; // Tap の場合はそのまま処理
+            }
+
+            // Hold の場合は Control 再現レイヤー扱いにする
+            if (record->event.pressed) {
+                layer_on(UPPER);
+            } else {
+                layer_off(UPPER);
+            }
+            return false;
+        case KC_BACKSLASH:
+            if (record->event.pressed) {
+                register_code(KC_INTERNATIONAL_3); // 日本語配列設定の Windows でキーコードと入力されるキーを一致させる
+            } else {
+                unregister_code(KC_INTERNATIONAL_3);
+            }
+            return false;
+        case KC_MISSION_CONTROL:
+            if (record->event.pressed) {
+                register_code(KC_LWIN);
+                register_code(KC_TAB);
+            } else {
+                unregister_code(KC_TAB);
+                unregister_code(KC_LWIN);
+            }
+            return false;
+        case KC_LAUNCHPAD:
+            if (record->event.pressed) {
+                register_code(KC_LWIN);
+            } else {
+                unregister_code(KC_LWIN);
+            }
+            return false;
+        default:
+            return true; // Process all other keycodes normally
+    }
+}
+
 // [Kounotori カスタマイズ]
 // https://github.com/qmk/qmk_firmware/blob/master/docs/tap_hold.md#hold-on-other-key-press
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LT(4, KC_BACKSLASH):
-        case LT(5, KC_SPACE):
-            return !(detected_host_os() == OS_MACOS || detected_host_os() == OS_IOS); // Windows のみ有効
         case LT(4, KC_GRAVE):
         case LCTL_T(KC_SPACE):
-            return (detected_host_os() == OS_MACOS || detected_host_os() == OS_IOS); // Mac のみ有効
+            return true;
         default:
             return false;
     }
